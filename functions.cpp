8cb8b5ce49f3d8c5366d9b49d744f203c05d861b
Khushi Kumari <khushikumari2392006@gmail.com>
2024-07-06 19:00:46 +0530

    Add files

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printname(string name){
+    cout <<"khushi_"<<name<<endl;
+}
+int main(){
+    string name;
+    cin>>name;
+    printname(name);
+
+    string name2;
+    cin>>name2;
+    printname(name2);
+
+}
