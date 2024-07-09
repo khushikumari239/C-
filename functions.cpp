16ecab635ef32ef12eb865b491658b6e0114713d
Khushi Kumari <khushikumari2392006@gmail.com>
2024-07-09 07:17:48 +0530

    Adding some new files

--- functions.cpp
+++ functions.cpp
@@ -1,16 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printname(string name){
-    cout <<"khushi_"<<name<<endl;
+// void printname(string name){
+//     cout <<"khushi_"<<name<<endl;
+// }
+
+
+void dosomething (int num){
+    cout<<num<<endl;
+    num+=6;
+    cout<<num<<endl;
+    num+=6;
+    cout<<num<<endl;
 }
 int main(){
-    string name;
-    cin>>name;
-    printname(name);
+    // string name;
+    // cin>>name;
+    // printname(name);
 
-    string name2;
-    cin>>name2;
-    printname(name2);
+    // string name2;
+    // cin>>name2;
+    // printname(name2);
 
+int num;
+cin>> num;
+dosomething(num);
+
+cout<<num<<endl; 
+
+return 0;
 }
---
+++ pattern1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+ using namespace std;
+
+/*printing start pattern 
+****
+****
+****
+**** 
+*/
+void print1(int n)
+{
+
+for (int i=0; i<n;i++)
+{
+    for(int j=0; j<n; j++)
+    {
+      cout<<"*";
+    }
+    cout<<endl;
+}
+
+}
+int main()
+{
+  int n;
+  cin>>n;
+
+print1(n); 
+// (n) passing the input n 
+
+    return 0;
+}
