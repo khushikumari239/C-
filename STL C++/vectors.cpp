be8d8a2dfca5d4a368ce4287aa03691ff565359b
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2024-09-10 18:34:33 +0530

    add new files

---
+++ STL C++/vectors.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+// int main () {
+
+//     return 0;
+// }
+
+
+
+
+void explainVector () {
+
+    vector< int > v;
+// vector is a container which is dynamic in nature u can change size when needed
+// vector stores elements in the similar manner as array 
+ v.push_back (1);
+ v.emplace_back (3);
+ 
+ 
+
+
+
+}
